3625f3963db1b80e41ee563e8b27af1cf7b47689
Ganeshp007 <potdart40.gp@gmail.com>
2022-05-10 14:03:39 +0530

    Added: UC1_CreateSimpleLinkedList

---
+++ Day-14-LinkedList/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_14_LinkedList
+{
+    internal class Node
+    {
+        public int data; //instance variable
+        public Node next; //Referance variable
+
+        public Node(int data)  //Default Constructor of Node class
+        {
+            this.data = data;   // init of instance variable
+        } 
+
+    }
+}
